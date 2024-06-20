1cecb72043d529a327dd97e60d032ae6dbcb2b36
ivannnnn <ivan2100736@gmail.com>
2024-06-20 17:48:59 +0800

    chore: remove useless codes

--- canvas.js
+++ canvas.js
@@ -1,63 +1,4 @@
-// // Canvas DOM 元素
-// const canvas = document.getElementById('Canvas');
-// const ctx = canvas.getContext('2d');
 
-// // 起始點座標
-// let x1 = 0;
-// let y1 = 0;
-
-// // 終點座標
-// let x2 = 0;
-// let y2 = 0;
-
-// // 宣告一個 hasTouchEvent 變數，來檢查是否有 touch 的事件存在
-// const hasTouchEvent = window.ontouchstart ? true : false;
-
-// // 透過上方的 hasTouchEvent 來決定要監聽的是 mouse 還是 touch 的事件
-// const downEvent = hasTouchEvent ? 'touchstart' : 'mousedown';
-// const moveEvent = hasTouchEvent ? 'touchmove' : 'mousemove';
-// const upEvent = hasTouchEvent ? 'touchend' : 'mouseup';
-
-// // 宣告 isMouseActive 為滑鼠點擊的狀態，因為我們需要滑鼠在 mousedown 的狀態時，才會監聽 mousemove 的狀態
-// let isMouseActive = false;
-// const canvasTop = canvas.offsetTop;
-// canvas.addEventListener(downEvent, function (e) {
-//   isMouseActive = true;
-//   // 根據事件類型取得座標
-//   x1 = hasTouchEvent ? e.touches[0].clientX : e.offsetX;
-//   y1 = hasTouchEvent ? e.touches[0].clientY - canvasTop : e.offsetY;
-//   // 比刷大小
-//   ctx.lineWidth = lineWidth;
-//   ctx.lineCap = 'round';
-//   ctx.lineJoin = 'round';
-// });
-
-// canvas.addEventListener(moveEvent, function (e) {
-//   if (!isMouseActive) {
-//     return;
-//   }
-//   // 取得終點座標
-//   x2 = hasTouchEvent ? e.touches[0].clientX : e.offsetX;
-//   y2 = hasTouchEvent ? e.touches[0].clientY - canvasTop : e.offsetY;
-
-//   // 開始繪圖
-//   ctx.beginPath();
-//   ctx.moveTo(x1, y1);
-//   ctx.lineTo(x2, y2);
-//   ctx.stroke();
-
-//   // 更新起始點座標
-//   x1 = x2;
-//   y1 = y2;
-// });
-
-// canvas.addEventListener(upEvent, function (e) {
-//   isMouseActive = false;
-// });
-
-// window.addEventListener(mouseup, function (e) {
-//   state.mousedown = false;
-// })
 // =============
 // == Globals ==
 // =============
@@ -87,7 +28,6 @@ canvas.addEventListener('touchstart', handleWritingStart);
 canvas.addEventListener('touchmove', handleWritingInProgress);
 canvas.addEventListener('touchend', handleDrawingEnd);
 window.addEventListener('touchend', handleDrawingEnd);
-// canvasWrapper.addEventListener('resize', resizeCanvas);
 
 
 // ====================
